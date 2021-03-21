2b923267d9c74b647f145fdd3c7c8d78342ab1af
sebi <sebimih@gmail.com>
2021-03-21 12:29:10 +0200

    Table

--- Cellular Automata/main.cpp
+++ Cellular Automata/main.cpp
@@ -1,8 +1,283 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "ResourceManager.h"
+
+// callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_cursor_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+// utility functions
+void Init();
+void DrawTable();
+
+// window settings
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 800;
+
+// time variables
+double deltaTime = 0.0f;		// time between current frame and last frame
+double lastFrame = 0.0f;		// time of last frame
+
+// table settings
+const int TABLE_WIDTH = 100;
+const int TABLE_HEIGHT = 70;
+const int MAX_ZOOM = 50;
+const int MIN_ZOOM = 8;
+int SquareSize = 10;
+
+// cursor settings
+GLFWcursor* HandCursor;
+
+// perspective coordinates
+int PerspectiveX = 0;
+int PerspectiveY = 0;
+
+// mouse
+bool IsMouseMoving;
+double LastX, LastY;
 
 int main()
 {
-	std::cout << "Hello";
+	// glfw: initialize and configure
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	// glfw window creation
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Cellular Automata", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_cursor_callback);
+	glfwSetScrollCallback(window, scroll_callback); 
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	// glad: load all OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// configure global OpenGL state
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// cursor shape
+	HandCursor = glfwCreateStandardCursor(GLFW_HAND_CURSOR);
+
+
+	Init();
+
+	
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// time
+		double currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+
+		// input
+		processInput(window);
+
+
+		// rendering commands here
+		glClearColor(0.83137f, 0.82353f, 0.83137f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		
+		DrawTable();
+
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources
+	glfwTerminate();
 	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//														Callbacks
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void mouse_cursor_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (IsMouseMoving)
+	{
+		double xoffset = xpos - LastX;
+		double yoffset = LastY - ypos;
+		LastX = xpos;
+		LastY = ypos;
+
+		PerspectiveX += (int)xoffset;
+		PerspectiveY -= (int)yoffset;
+	}
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	SquareSize += (int)yoffset;
+
+	if (SquareSize < MIN_ZOOM)
+		SquareSize = MIN_ZOOM;
+
+	if (SquareSize > MAX_ZOOM)
+		SquareSize = MAX_ZOOM;
+
+	std::cout << SquareSize << '\n';
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		glfwGetCursorPos(window, &LastX, &LastY);
+		IsMouseMoving = true;
+		glfwSetCursor(window, HandCursor);
+	}
+	else
+	{
+		IsMouseMoving = false;
+		glfwSetCursor(window, NULL);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//														Utility functions
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+GLuint LineVAO, QuadVAO;
+void Init()
+{
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//													Configure VAO/VBO
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// LINE
+	float LineVertices[] = {
+		0.0f, 0.0f,			// row left
+		1.0f, 0.0f,			// row right
+	};
+
+	GLuint LineVBO;
+	glGenVertexArrays(1, &LineVAO);
+	glGenBuffers(1, &LineVBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, LineVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(LineVertices), LineVertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(LineVAO);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+	// QUAD
+	float QuadVertices[] = {
+		0.0f, 0.0f,
+		1.0f, 1.0f,
+		0.0f, 1.0f,
+
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		1.0f, 1.0f
+	};
+
+	GLuint QuadVBO;
+	glGenVertexArrays(1, &QuadVAO);
+	glGenBuffers(1, &QuadVBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, QuadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(QuadVertices), QuadVertices, GL_STATIC_DRAW);
+
+	glBindVertexArray(QuadVAO);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//														Shaders
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// load shaders
+	ResourceManager::LoadShader("shaders/line.vert", "shaders/line.frag", nullptr, "line");
+
+	// configure shader
+	glm::mat4 projection = glm::ortho(0.0f, (float)SCR_WIDTH, (float)SCR_HEIGHT, 0.0f);
+
+	ResourceManager::GetShader("line").Use();
+	ResourceManager::GetShader("line").SetMatrix4f("projection", projection);
+}
+
+void DrawTable()
+{
+	// TODO: DEBUG
+	// std::cout << "Perspective : " << PerspectiveX << ' ' << PerspectiveY << '\n';
+
+
+	glm::mat4 model = glm::mat4(1.0f);
+
+	// draw rows and columns
+	glBindVertexArray(LineVAO);
+	ResourceManager::GetShader("line").Use();
+	ResourceManager::GetShader("line").SetVector3f("color", glm::vec3(0.0f, 0.0f, 0.0f));
+
+	// draw rows
+	for (int y = PerspectiveY; y <= PerspectiveY + TABLE_HEIGHT * SquareSize; y += SquareSize)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(PerspectiveX, y, 0.0f));
+		model = glm::scale(model, glm::vec3((float)(TABLE_WIDTH * SquareSize), 0.0f, 0.0f));
+		ResourceManager::GetShader("line").SetMatrix4f("model", model);
+
+		glDrawArrays(GL_LINES, 0, 2);
+	}
+
+	// draw columns
+	for (int x = PerspectiveX; x <= PerspectiveX + TABLE_WIDTH * SquareSize; x += SquareSize)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(x, PerspectiveY, 0.0f));
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3((float)(TABLE_HEIGHT * SquareSize), 0.0f, 0.0f));
+		ResourceManager::GetShader("line").SetMatrix4f("model", model);
+
+		glDrawArrays(GL_LINES, 0, 2);
+	}
+
+	glBindVertexArray(0);
 }
 
